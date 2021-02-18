b5599ef49b77d2dea98ffc69baa864457f64fd27
Arjun Sarao <47152801+DarkHawk727@users.noreply.github.com>
2021-02-18 11:28:22 -0500

    Create J1 - NextinLine.java

---
+++ CCC 2013 Junior/J1 - NextinLine.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+public class Main
+{
+  public static void main (String[]args)
+  {
+    BufferedReader input = new BufferedReader (new InputStreamReader (System.in));
+    int y = Integer.parseInt (input.readLine ());
+    int m = Integer.parseInt (input.readLine ());
+    int diff = m - y;
+    int oldest = m + diff;
+      System.out.println (oldest);
+  }
+}
