05f123611038b425bcc56b69e4971d371ef8ba07
Arjun Sarao <47152801+DarkHawk727@users.noreply.github.com>
2021-03-18 17:54:18 -0400

    Create J1 - WhatisNDaddy.java

---
+++ CCC 2010/Junior/J1 - WhatisNDaddy.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(input.readLine());
+        if (n == 1) {
+            System.out.println(1);
+        }
+        if (n == 2) {
+            System.out.println(2);
+        }
+        if (n == 3) {
+            System.out.println(2);
+        }
+        if (n == 4) {
+            System.out.println(3);
+        }
+        if (n == 5) {
+            System.out.println(3);
+        }
+        if (n == 6) {
+            System.out.println(3);
+        }
+        if (n == 7) {
+            System.out.println(2);
+        }
+        if (n == 8) {
+            System.out.println(2);
+        }
+        if (n == 9) {
+            System.out.println(1);
+        }
+        if (n == 10) {
+            System.out.println(1);
+        }
+    }
+}
