a8982081908b6dc2006c12640df8ed319153bb35
Arjun Sarao <47152801+DarkHawk727@users.noreply.github.com>
2021-11-18 16:29:25 -0500

    Create J3-Icon_Scaling.py

---
+++ CCC 2012/Junior/J3-Icon_Scaling.py
@@ -0,0 +1,13 @@
+def scale_icon(k):
+	for i in range(k):
+			print("*"*k + "x"*k + "*"*k)
+
+	for i in range(k):
+			print(" "*k + "x"*k + "x"*k)
+
+	for i in range(k):
+			print("*"*k + " "*k + "*"*k)
+
+scaling_factor = int(input())
+
+scale_icon(scaling_factor)
