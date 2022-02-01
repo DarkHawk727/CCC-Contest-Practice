432ae90430b038462b1c9496feffc95938471ca5
Arjun Sarao <arjunsarao727@gmail.com>
2022-02-01 12:37:20 -0500

    did another question

--- CCC 2004/Senior/S1-Fix.py
+++ CCC 2004/Senior/S1-Fix.py
@@ -4,5 +4,24 @@ input = sys.stdin.readline
 
 N = int(input())
 
-for _ in range(3*N):
-    print(N)
+
+def func(string1, string2):
+    len1, len2 = len(string1), len(string2)
+    if len2 > len1:
+        string1, string2 = string2, string1
+        len1, len2 = len2, len1
+    if string1[:len2] != string2 and string1[-len2:] != string2:
+        # print(string1[:len2], string2, string1[-len2:])
+        return True
+    else:
+        return False
+
+
+for _ in range(N):
+    string1 = input().strip()
+    string2 = input().strip()
+    string3 = input().strip()
+    if func(string1, string2) and func(string2, string3) and func(string1, string3):
+        print("Yes")
+    else:
+        print("No")
